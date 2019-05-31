3a81529d16d705c53545a155e968d1a166350b52
Eunju Chong <eun01300714@gmail.com>
2019-05-31 15:48:41 +0900

    #1 design form

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -29,39 +29,41 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.CbTeas = new System.Windows.Forms.ComboBox();
+            this.lblTeaName = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.count = new System.Windows.Forms.Timer(this.components);
+            this.txtCountDown = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // CbTeas
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(13, 13);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(131, 20);
-            this.comboBox1.TabIndex = 0;
+            this.CbTeas.FormattingEnabled = true;
+            this.CbTeas.Location = new System.Drawing.Point(13, 13);
+            this.CbTeas.Name = "CbTeas";
+            this.CbTeas.Size = new System.Drawing.Size(131, 20);
+            this.CbTeas.TabIndex = 0;
+            this.CbTeas.SelectedIndexChanged += new System.EventHandler(this.Teas_SelectedIndexChanged);
             // 
-            // label1
+            // lblTeaName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(150, 17);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.lblTeaName.AutoSize = true;
+            this.lblTeaName.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTeaName.Location = new System.Drawing.Point(150, 17);
+            this.lblTeaName.Name = "lblTeaName";
+            this.lblTeaName.Size = new System.Drawing.Size(0, 16);
+            this.lblTeaName.TabIndex = 1;
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(12, 63);
+            this.btnStart.Location = new System.Drawing.Point(13, 39);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(226, 73);
             this.btnStart.TabIndex = 2;
             this.btnStart.Text = "담그기!";
             this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
             // 
             // label2
             // 
@@ -76,18 +78,30 @@
             // count
             // 
             this.count.Interval = 1000;
+            this.count.Tick += new System.EventHandler(this.Count_Tick);
+            // 
+            // txtCountDown
+            // 
+            this.txtCountDown.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtCountDown.Location = new System.Drawing.Point(13, 119);
+            this.txtCountDown.Name = "txtCountDown";
+            this.txtCountDown.Size = new System.Drawing.Size(226, 26);
+            this.txtCountDown.TabIndex = 4;
+            this.txtCountDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(250, 184);
+            this.Controls.Add(this.txtCountDown);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblTeaName);
+            this.Controls.Add(this.CbTeas);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,11 +109,12 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CbTeas;
+        private System.Windows.Forms.Label lblTeaName;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer count;
+        private System.Windows.Forms.TextBox txtCountDown;
     }
 }
 
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -7,14 +7,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+        int[] TeaTime = new int[]{ 120, 180, 300 };
+        string[] TeaList = new string[]{ "홍차", "녹차", "루이보스차", "국화차" };
+        int CountTime = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < TeaList.Length; i++)
+                this.CbTeas.Items.Add(TeaList[i]);
+
+            if (this.CbTeas.Items.Count > 0)
+            {
+                this.CbTeas.SelectedIndex = 0; // select first tea
+                this.lblTeaName.Text = TeaList[0]; // selected first tea
+            }
+        }
+
+        private void Teas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lblTeaName.Text = this.CbTeas.Text;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            this.txtCountDown.ReadOnly = true; // can't write here
+            this.count.Enabled = true; // start timer
+            switch(CbTeas.Text)
+            {
+                case "홍차":
+                    CountTime = TeaTime[0];
+                    break;
+                case "녹차":
+                    CountTime = TeaTime[1];
+                    break;
+                case "루이보스차":
+                    CountTime = TeaTime[2];
+                    break;
+                case "국화차":
+                    CountTime = TeaTime[0];
+                    break;
+            }
+        }
+
+        private void Count_Tick(object sender, EventArgs e)
+        {
+            if(CountTime<1)
+            { // be time out
+                this.count.Enabled = false;
+                this.txtCountDown.Text = "";
+                MessageBox.Show("티백을 건지세요!", "시간이 끝났습니다.",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.CbTeas.Focus();
+            }
+            else
+            {
+                this.CountTime--;
+                int min = this.CountTime / 60;
+                int sec = this.CountTime % 60;
+                this.txtCountDown.Text = Convert.ToString(min)+"분 "+Convert.ToString(sec)+"초 남았습니다!";
+            }
         }
     }
 }
